710b54d3c675ca72593a18c1ca4d74de04c15507
ADANI\zabrodin <rediskal50@gmail.com>
2025-06-24 15:14:07 +0300

    ref: add properties for User secrets

---
+++ JarvisBot/Background/JarvisClientSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JarvisBot.Background
+{
+    public class JarvisClientSettings
+    {
+        public string? TelegramBotClient { get; set; }
+        public int AdminChatId { get; set; }
+        public string? YandexKey { get; set; }
+        public string? AlfabankRate { get; set; }
+        public string? YandexWeather { get; set; }
+
+        public string? OldExchangeRatesPath { get; set; }
+    }
+}
